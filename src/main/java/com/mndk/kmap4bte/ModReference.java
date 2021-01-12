6e813dfa9454696444b82c82cac1eca7f44788df
tf2_mandeokyi <askminjae@gmail.com>
2021-01-12 21:47:50 +0900

    v1.0 -> v1.01

--- src/main/java/com/mndk/kmap4bte/ModReference.java
+++ src/main/java/com/mndk/kmap4bte/ModReference.java
@@ -3,5 +3,5 @@ package com.mndk.kmap4bte;
 public class ModReference {
     public static final String MODID = "kmap4bte";
     public static final String NAME = "Korean Maps For BTE";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.01";
 }
