9d783fa7e3f60515021efeaf44db58a3115b392a
tf2_mandeokyi <askminjae@gmail.com>
2021-03-23 20:37:41 +0900

    Add "Category" to selector UI

--- src/main/java/com/mndk/bte_tr/gui/sub_ui/MapSelectorUI.java
+++ src/main/java/com/mndk/bte_tr/gui/sub_ui/MapSelectorUI.java
@@ -1,6 +1,8 @@
 package com.mndk.bte_tr.gui.sub_ui;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.mndk.bte_tr.BTETerraRenderer;
 import com.mndk.bte_tr.config.ConfigHandler;
@@ -20,6 +22,8 @@ public class MapSelectorUI extends GuiSubScreen {
 
 	static final int COMPONENT_ID_GROUP = 200;
 	
+	private static List<Object> clickableElementList = new ArrayList<>();
+	
 	private static final int TITLE_MARGIN_BOTTOM = 20;
 	
 	private static int LIST_WIDTH;
@@ -31,14 +35,10 @@ public class MapSelectorUI extends GuiSubScreen {
     private static final int LIST_LEFT = DefaultMapRenderingOptionsUI.BASIC_OPTIONS_WIDTH + DefaultMapRenderingOptionsUI.BASIC_OPTIONS_MARGIN_LEFT
     		+ LIST_LEFT_MARGIN;
 
-    // private static final int DONE_BUTTON_BOTTOM_MARGIN = 26;
-
     
     
     private static final ResourceLocation RADIO_BUTTON_IMAGE =
             new ResourceLocation(BTETerraRenderer.MODID, "textures/ui/radio_button.png");
-
-    // private GuiButton doneButton;
     
     
 	
@@ -54,23 +54,18 @@ public class MapSelectorUI extends GuiSubScreen {
 		
 		int tempWidth;
 		LIST_WIDTH = 0;
+		clickableElementList.clear();
+		
 		for(TileMapJsonResult.Category category : TileMapJsonLoader.result.getCategories()) {
+			clickableElementList.add(category.getName());
 			for(ExternalTileMap map : category.getMaps()) {
+				clickableElementList.add(map);
 				tempWidth = this.fontRenderer.getStringWidth(map.getName());
 				LIST_WIDTH = LIST_WIDTH < tempWidth ? tempWidth : LIST_WIDTH;
 			}
 		}
 		
-		LIST_WIDTH += 2 * LIST_PADDING + 20;
-		
-		/*
-		doneButton = new GuiButton(
-				COMPONENT_ID_GROUP,
-				LIST_LEFT + LIST_PADDING, parent.height - DONE_BUTTON_BOTTOM_MARGIN,
-				LIST_WIDTH - 2 * LIST_PADDING, MapRenderingOptionsUI.DEFAULT_BUTTON_HEIGHT,
-                I18n.format("gui.done")
-        );
-		*/
+		LIST_WIDTH += Math.max(120, LIST_WIDTH + 2 * LIST_PADDING + 20);
 	}
 	
 	
@@ -91,7 +86,7 @@ public class MapSelectorUI extends GuiSubScreen {
 		int c = LIST_TOP_MARGIN + LIST_PADDING + this.fontRenderer.FONT_HEIGHT + TITLE_MARGIN_BOTTOM;
 		int h = this.fontRenderer.FONT_HEIGHT + ELEMENT_TOP_MARGIN;
 		
-		Gui.drawRect(LIST_LEFT, LIST_TOP_MARGIN, LIST_LEFT + LIST_WIDTH, c + h * 100, 0x3F000000); // TODO change 100 to something else
+		Gui.drawRect(LIST_LEFT, LIST_TOP_MARGIN, LIST_LEFT + LIST_WIDTH, c + h * clickableElementList.size(), 0x3F000000);
 		
 		this.drawCenteredString(
                 this.fontRenderer, I18n.format("gui.bte_tr.maprenderer.map_source"),
@@ -99,24 +94,35 @@ public class MapSelectorUI extends GuiSubScreen {
         );
 		
 		int i = 0;
-		for(TileMapJsonResult.Category category : TileMapJsonLoader.result.getCategories()) {
-			for(ExternalTileMap map : category.getMaps()) {
-	            float u = (ModConfig.currentMapManager.getId().equals(map.getId()) ? 1/8.f : 0) + (isMouseOnIndex(mouseX, mouseY, i) ? 1/16.f : 0);
-	            ImageUiRenderer.drawImage(RADIO_BUTTON_IMAGE,
+		for(Object object : clickableElementList) {
+			if(object instanceof String) {
+				String categoryName = (String) object;
+				this.drawCenteredString(this.fontRenderer, categoryName,
+	            		LIST_LEFT + LIST_WIDTH / 2,
+	                    c + h * i - (this.fontRenderer.FONT_HEIGHT / 2),
+	                    0xFFFFFF
+	            );
+			}
+			else if(object instanceof ExternalTileMap) {
+				
+				ExternalTileMap map = (ExternalTileMap) object;
+				float u = (ModConfig.currentMapManager.getId().equals(map.getId()) ? 1/8.f : 0) + (isMouseOnIndex(mouseX, mouseY, i) ? 1/16.f : 0);
+				
+				ImageUiRenderer.drawImage(RADIO_BUTTON_IMAGE,
 	            		LIST_LEFT + LIST_PADDING,
 	                    c + h * i - 8,
 	                    0,
 	                    16, 16,
-	                    u, 0, u + 1/16.f, 1/16.f);
+	                    u, 0, u + 1/16.f, 1/16.f
+	            );
 	            this.drawString(this.fontRenderer, map.getName(),
 	            		LIST_LEFT + LIST_PADDING + 20,
 	                    c + h * i - (this.fontRenderer.FONT_HEIGHT / 2),
-	                    0xFFFFFF);
-	            i++;
+	                    0xFFFFFF
+	            );
 			}
+			++i;
 		}
-
-        // doneButton.drawButton(parent.mc, mouseX, mouseY, partialTicks);
 	}
 	
 	
@@ -136,18 +142,27 @@ public class MapSelectorUI extends GuiSubScreen {
 	
 	
 	
+	private int getMouseIndex(int mouseX, int mouseY) {
+		if(mouseX < LIST_LEFT + LIST_PADDING || mouseX > LIST_LEFT + LIST_WIDTH - LIST_PADDING) return -1;
+		
+		int c = LIST_TOP_MARGIN + LIST_PADDING + this.fontRenderer.FONT_HEIGHT + TITLE_MARGIN_BOTTOM;
+		int h = this.fontRenderer.FONT_HEIGHT + ELEMENT_TOP_MARGIN;
+		int index = (int) Math.round((mouseY - c) / (double) h), y = c + h * (int) index;
+		
+		if(index < 0 || index >= clickableElementList.size()) return -1;
+		if(mouseY - y < -8 || mouseY - y > 8) return -1; // If the cursor is at the gap between elements
+		return index;
+	}
+	
+	
+	
 	@Override
 	public void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-		int i = 0;
-		for(TileMapJsonResult.Category category : TileMapJsonLoader.result.getCategories()) {
-			for(ExternalTileMap map : category.getMaps()) {
-	            if(isMouseOnIndex(mouseX, mouseY, i)) {
-	                ConfigHandler.getModConfig().setMapId(map.getId());
-	                return;
-	            }
-	            i++;
-	        }
-		}
+		int index = this.getMouseIndex(mouseX, mouseY);
+		if(index == -1) return;
+		Object obj = clickableElementList.get(index);
+		if(!(obj instanceof ExternalTileMap)) return;
+		ConfigHandler.getModConfig().setMapId(((ExternalTileMap) obj).getId());
 	}
 	
 	
