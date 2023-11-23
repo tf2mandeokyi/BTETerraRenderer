a7afccd23c8033f3e93a3bd0a3161bdfdc6042d6
m4ndeokyi <askminjae@gmail.com>
2023-11-23 15:49:02 +0900

    Fix cache system not working properly? Idk

--- core/src/main/java/com/mndk/bteterrarenderer/dep/terraplusplus/HttpResourceManager.java
+++ core/src/main/java/com/mndk/bteterrarenderer/dep/terraplusplus/HttpResourceManager.java
@@ -1,13 +1,15 @@
 package com.mndk.bteterrarenderer.dep.terraplusplus;
 
-import com.mndk.bteterrarenderer.core.util.mixin.MixinUtil;
+import com.mndk.bteterrarenderer.dep.terraplusplus.http.Http;
+import io.netty.buffer.ByteBufInputStream;
 import lombok.experimental.UtilityClass;
 
 import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
 
 @UtilityClass
 public class HttpResourceManager {
-    public InputStream download(String url) {
-        return MixinUtil.notOverwritten(url);
+    public InputStream download(String url) throws ExecutionException, InterruptedException {
+        return new ByteBufInputStream(Http.get(url).get());
     }
 }
